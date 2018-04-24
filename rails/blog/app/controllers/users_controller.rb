0559c8d61bb3a486d11e8ef139b6d61f452fd4fd
汪波 <wangbo@giabbs.com>
2018-04-24 15:36:49 +0800

    Update users_controller.rb

--- rails/blog/app/controllers/users_controller.rb
+++ rails/blog/app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params # 用于过滤传入的参数
     # password_confirmation是用来校对密码是否相同，同时为密码加密
     params.require(:user).permit(
